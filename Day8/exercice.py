aaf5f3ffd19ee26a555140895daa69fdac112e34
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2022-12-09 22:44:04 +0100

    Day8 and Day 9

--- Day8/exercice.py
+++ Day8/exercice.py
@@ -9,35 +9,17 @@ dir = {"UP": -1, "DOWN": 1, "RIGHT": 1, "LEFT": -1}
 def is_visible(map:list, tree_coords:tuple, direction:str):
     # Coords            Y     and       X
     value = map[tree_coords[1]][tree_coords[0]] 
-    print("TESTING : (",tree_coords[0]," ",tree_coords[1], ") with value =>", value, "and dir = ", direction)
     if (direction == "UP" and tree_coords[1] == 0) or (direction == "DOWN" and tree_coords[1] == len(map)-1) or (direction == "LEFT" and tree_coords[0] == 0) or (direction == "RIGHT" and tree_coords[0] == len(map[0])-2):
-        print("exeptions")
         return True
-    if direction == "UP":
+    if direction == "UP" or direction == "DOWN":
         counter = tree_coords[1] + dir[direction]
-        while (counter >= 0):
-            print(counter)
+        while (counter >= 0) and counter <= len(map)-1:
             if (value <= map[counter][tree_coords[0]]):
                 return False
             counter += dir[direction]
-    elif direction == "DOWN":
-        counter = tree_coords[1] + dir[direction]
-        while (counter <= len(map)-1):
-            print(counter)
-            if (value <= map[counter][tree_coords[0]]):
-                return False
-            counter += dir[direction]
-    elif direction == "LEFT":
+    elif direction == "LEFT" or direction == "RIGHT":
         counter = tree_coords[0] + dir[direction]
-        while (counter >= 0):
-            print("x = ", counter, "y = ", tree_coords[1])
-            if (value <= map[tree_coords[1]][counter]):
-                return False
-            counter += dir[direction]
-    elif direction == "RIGHT":
-        counter = tree_coords[0]+dir[direction]
-        while (counter <= len(map[0])-2):
-            print("x = ", counter, "y = ", tree_coords[1])
+        while (counter >= 0 and counter <= len(map[0])-2):
             if (value <= map[tree_coords[1]][counter]):
                 return False
             counter += dir[direction]
@@ -46,38 +28,19 @@ def is_visible(map:list, tree_coords:tuple, direction:str):
 def scenic_score(map:list, tree_coords:tuple, direction:str):
     # Coords            Y     and       X
     value = map[tree_coords[1]][tree_coords[0]] 
-    print("TESTING : (",tree_coords[0]," ",tree_coords[1], ") with value =>", value, "and dir = ", direction)
     if (direction == "UP" and tree_coords[1] == 0) or (direction == "DOWN" and tree_coords[1] == len(map)-1) or (direction == "LEFT" and tree_coords[0] == 0) or (direction == "RIGHT" and tree_coords[0] == len(map[0])-2):
         return 1
     real = 1
-    if direction == "UP":
+    if direction == "UP" or direction == "DOWN":
         counter = tree_coords[1] + dir[direction]
-        while (counter >= 0):
-            print(counter)
+        while (counter >= 0 and counter <= len(map)-1):
             if (value <= map[counter][tree_coords[0]]):
                 return real
             counter += dir[direction]
             real += 1
-    elif direction == "DOWN":
-        counter = tree_coords[1] + dir[direction]
-        while (counter <= len(map)-1):
-            print(counter)
-            if (value <= map[counter][tree_coords[0]]):
-                return real
-            counter += dir[direction]
-            real += 1
-    elif direction == "LEFT":
+    elif direction == "LEFT" or direction == "RIGHT":
         counter = tree_coords[0] + dir[direction]
-        while (counter >= 0):
-            print("x = ", counter, "y = ", tree_coords[1])
-            if (value <= map[tree_coords[1]][counter]):
-                return real
-            counter += dir[direction]
-            real += 1
-    elif direction == "RIGHT":
-        counter = tree_coords[0]+dir[direction]
-        while (counter <= len(map[0])-2):
-            print("x = ", counter, "y = ", tree_coords[1])
+        while (counter >= 0 and counter <= len(map[0])-2):
             if (value <= map[tree_coords[1]][counter]):
                 return real
             counter += dir[direction]
@@ -85,41 +48,23 @@ def scenic_score(map:list, tree_coords:tuple, direction:str):
     return real - 1
 
 def question_1(parsed_data:list) -> None:
-    print(parsed_data[0][2])
-    a = 0
     counter = 0
     for y in range(len(parsed_data)):
         for x in range(len(parsed_data[0])-1):
-            print("Y=", y, ' X=', x)
-            a += 1
             up = is_visible(parsed_data, (x,y), "UP")
             down = is_visible(parsed_data, (x,y), "DOWN")
             left = is_visible(parsed_data, (x,y), "LEFT")
             right = is_visible(parsed_data, (x,y), "RIGHT")
-            print("UP : ", up, " | DOWN : ", down, " | LEFT : ", left, " | RIGHT : ", right)
             if up or down or left or right:
                 counter += 1
-    print(a)
-    print(len(parsed_data[0])*len(parsed_data))
     return counter
 
 def question_2(parsed_data:list) -> None:
-    print(parsed_data[0][2])
-    a = 0
     scores = []
     for y in range(len(parsed_data)):
         for x in range(len(parsed_data[0])-1):
-            print("Y=", y, ' X=', x)
-            a += 1
-            up = scenic_score(parsed_data, (x,y), "UP")
-            down = scenic_score(parsed_data, (x,y), "DOWN")
-            left = scenic_score(parsed_data, (x,y), "LEFT")
-            right = scenic_score(parsed_data, (x,y), "RIGHT")
-            print("UP : ", up, " | DOWN : ", down, " | LEFT : ", left, " | RIGHT : ", right)
-            print("Score = ", up*down*left*right)
-            scores.append(up*down*left*right) 
-    print(a)
-    print(len(parsed_data[0])*len(parsed_data))
+            scores.append(scenic_score(parsed_data, (x,y), "UP")*scenic_score(parsed_data, (x,y), "DOWN")*scenic_score(parsed_data, (x,y), "LEFT")*scenic_score(parsed_data, (x,y), "RIGHT")) 
     return max(scores)
-#print(question_1(parse_data("data")))
-print(question_2(parse_data("data")))
+
+print("/ Question 1 \\\nNb visible = ", question_1(parse_data("data")))
+print("/ Question 2 \\\nMax score = ", question_2(parse_data("data")))
--- Day9/exercice.py
+++ Day9/exercice.py
@@ -6,74 +6,44 @@ def parse_data(filepath:str) -> list:
 
 dir = {"U":1, "D":-1, "R":1, "L":-1}
 
-def get_tail_position(head_position:list, tail_position:list, direction:str, last_pos):
-    if direction == "UP" or direction == "DOWN":
-        if head_position[0] == tail_position[0] and head_position != [tail_position[0], tail_position[1]+dir[direction]]:
-            print("move direcly")
-            return [tail_position[0], tail_position[1]+dir[direction]]
-        else:
-            return last_pos
-        """elif head_position[0] < head_position[0] and head_position != [tail_position[0]+1, tail_position[1]+dir[direction]]: # + verifier si le mouv -/-> tail sur head
-            print("cond")
-            return [tail_position[0]+1, tail_position[1]+dir[direction]]
-        elif head_position[0] > head_position[0] and head_position != [tail_position[0]-1, tail_position[1]+dir[direction]]:
-            print("cond")
-            return [tail_position[0]-1, tail_position[1]+dir[direction]]
-        else:
-            print("don't move")
-            return tail_position #Tail doesn't move"""
-    else:
-        if head_position[1] == tail_position[1] and head_position != [tail_position[0]+dir[direction], tail_position[1]]:
-            print("move direcly")
-            return [tail_position[0]+dir[direction], tail_position[1]]
-        else:
-            return last_pos
-        """elif head_position[1] < head_position[1] and head_position != [tail_position[0]+dir[direction], tail_position[1]-1]:
-            print("cond")
-            return [tail_position[0]+dir[direction], tail_position[1]-1]
-        elif head_position[1] > head_position[1] and head_position != [tail_position[0]+dir[direction], tail_position[1]-1]:
-            print("cond")
-            return [tail_position[0]+dir[direction], tail_position[1]+1]
-        else:
-            print("don't move")
-            return tail_position"""
 
-def question_1(parsed_data:list):
+def get_tail_position(head_position:list, tail_position:list):
+    if abs(tail_position[0] - head_position[0]) == 2 and abs(tail_position[1] - head_position[1]) == 2:
+        return [tail_position[0] + int((head_position[0]-tail_position[0]) / 2), tail_position[1] + int((head_position[1]-tail_position[1]) / 2)]
+    elif abs(tail_position[0] - head_position[0]) == 2:
+        return [tail_position[0] + int((head_position[0]-tail_position[0]) / 2), head_position[1]]
+    elif abs(tail_position[1] - head_position[1]) == 2:
+        return [head_position[0], tail_position[1] + int((head_position[1]-tail_position[1]) / 2)]
+    else:
+        return tail_position
+
+
+def questions(parsed_data:list, rope_len:int):
     head_pos = [0,0]
-    head_last_pos = [0,0]
-    tail_pos = [0,0]
+    nodes_pos = []
+    for i in range(rope_len):
+        nodes_pos.append([0,0])
     visited_positions = {}
     for lines in parsed_data:
         line = lines.split(" ")
         direction = line[0]
         step = int(line[1])
         for i in range(step):
-            print("\n")
             if direction == "U":
-                head_last_pos = head_pos
                 head_pos = [head_pos[0], head_pos[1]+1]
-                tail_pos = get_tail_position(head_pos, tail_pos, "U", head_last_pos)
-                print("up")
             elif direction == "D":
-                head_last_pos = head_pos
                 head_pos = [head_pos[0], head_pos[1]-1]
-                tail_pos = get_tail_position(head_pos, tail_pos, "D", head_last_pos)
-                print("down")
             elif direction == "L":
-                head_last_pos = head_pos
                 head_pos = [head_pos[0]-1, head_pos[1]]
-                tail_pos = get_tail_position(head_pos, tail_pos, "L", head_last_pos)
-                print("left")
             else:
-                head_last_pos = head_pos
                 head_pos = [head_pos[0]+1, head_pos[1]]
-                tail_pos = get_tail_position(head_pos, tail_pos, "R", head_last_pos)
-                print("right")
-            visited_positions[tuple(tail_pos)] = True
-            print(line)
-            print(head_pos)
-            print(tail_pos)
-    return visited_positions
+            for i in range(len(nodes_pos)):
+                if i == 0:
+                    nodes_pos[i] = get_tail_position(head_pos, nodes_pos[i])
+                else:
+                    nodes_pos[i] = get_tail_position(nodes_pos[i-1], nodes_pos[i])
+            visited_positions[tuple(nodes_pos[len(nodes_pos)-1])] = True
+    return len(visited_positions)
 
-dict = question_1(parse_data("data"))
-print(len(dict))
+print("/ Question 1 \\\nTail visited cells = ", questions(parse_data("data"), 1))
+print("/ Question 2 \\\nTail visited cells = ", questions(parse_data("data"), 9))
